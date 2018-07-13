237bd0cbde2e2cc2592aa680607e92a8d54796fd
3ypup <posilay.pochashe@gmail.com>
2018-07-13 14:55:14 +0300

    styles and gallery added

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,12 @@
 class Post < ApplicationRecord
+
+  validates :title, presence: true 
+  validates :title, :length => {:minimum => 3, :maximum => 20} 
+  validates :author, presence: true
+  validates :content, presence: true
+  validates :content, :length => {:minimum => 10, :maximum => 2000}
+  validates :image, presence: true
+  validates_format_of :image,  :with => %r{(png|jpg|jpeg)}i, :message => "Неправильный формат файла !!"
+
+
 end
