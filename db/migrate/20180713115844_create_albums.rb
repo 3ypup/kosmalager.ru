55c57e302860113013bd4d6a083ff9403e72a550
3ypup <posilay.pochashe@gmail.com>
2018-07-13 16:58:36 +0300

    albums added

---
+++ db/migrate/20180713115844_create_albums.rb
@@ -0,0 +1,14 @@
+class CreateAlbums < ActiveRecord::Migration[5.2]
+  def change
+    create_table :albums do |t|
+
+      t.text :author
+      t.text :content
+      t.text :title
+      t.string :image
+
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_13_070746) do
+ActiveRecord::Schema.define(version: 2018_07_13_115844) do
+
+  create_table "albums", force: :cascade do |t|
+    t.text "author"
+    t.text "content"
+    t.text "title"
+    t.string "image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.text "author"
