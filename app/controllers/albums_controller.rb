020248c14db03ed9adf189c74b7d5fad172ec554
3ypup <posilay.pochashe@gmail.com>
2018-07-16 14:12:08 +0300

    devise integrated

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -1,13 +1,22 @@
 class AlbumsController < ApplicationController
 
+
+
+
 def index
+
 @bckg = "home.jpg"
 @title = "Галерея"
 @label = "Храма свв. бесср. Космы и Дамиана"
+
+
 end
 
 
 def new
+@bckg = "home.jpg"
+@title = "Галерея"
+@label = "Храма свв. бесср. Космы и Дамиана"
 
 end
 
