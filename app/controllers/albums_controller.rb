0ee708eaec288dd38bc915ff6d61f574b6606fab
3ypup <posilay.pochashe@gmail.com>
2018-07-16 16:15:33 +0300

    added albums show

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -10,6 +10,8 @@ def index
 @label = "Храма свв. бесср. Космы и Дамиана"
 
 
+@albums=Album.all
+
 end
 
 
@@ -17,6 +19,52 @@ def new
 @bckg = "home.jpg"
 @title = "Галерея"
 @label = "Храма свв. бесср. Космы и Дамиана"
+
+end
+
+
+
+def create
+@bckg = "home.jpg"
+@title = "Галерея"
+@label = "Храма свв. бесср. Космы и Дамиана"
+
+@album = Album.new(album_params)
+      
+      if  @album.save
+        redirect_to @album
+      else
+        render action: 'new'
+      end
+  end
+
+
+def show
+@album = Album.find(params[:id])
+@bckg = "home.jpg"
+
+@title = @album.title
+
+@label = @album.content
+  
+  
+
+end
+
+
+def destroy
+  @album = Album.find(params[:id])
+  
+  @album.comments.each do |comment|
+    comment.destroy
+  end
+
+  
+
+  @album.destroy
+
+
+  redirect_to albums_path
 
 end
 
--- app/models/album.rb
+++ app/models/album.rb
@@ -1,2 +1,12 @@
 class Album < ApplicationRecord
+  
+
+  validates :title, presence: true 
+  validates :title, :length => {:minimum => 3, :maximum => 20} 
+  validates :author, presence: true
+  
+  validates :image, presence: true
+  validates_format_of :image,  :with => %r{(png|jpg|jpeg)}i, :message => "Неправильный формат файла !!"
+
+  mount_uploader :image, AlbumimgUploader
 end
