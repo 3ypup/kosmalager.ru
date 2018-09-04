3c6bf0efed027a68350d73aac36821a4160227aa
3ypup <posilay.pochashe@gmail.com>
2018-09-04 15:57:53 +0300

    formstyle fix

--- app/models/album.rb
+++ app/models/album.rb
@@ -2,7 +2,7 @@ class Album < ApplicationRecord
   has_many :pics
 
   validates :title, presence: { :message => "Выберите название альбома!!!"} 
-  validates :title, :length => {:minimum => 3, :maximum => 20, :message => "Слишком длинное название альбома!!!"} 
+  validates :title, :length => { :maximum => 20, :message => "Слишком длинное название альбома!!!"} 
   validates :author, presence: true
   
   validates :image, presence: {:message => "Выберите обложку альбома!!!"}
