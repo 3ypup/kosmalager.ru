23fd420e8844b48b56558167296d45cb4f274335
3ypup <posilay.pochashe@gmail.com>
2018-09-10 09:44:02 +0300

    max content of post fixed

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,7 +4,7 @@ class Post < ApplicationRecord
   validates :title, :length => { :maximum => 40, :message => "Слишком длинная тема поста !!"}
   validates :author, presence: true
   validates :content, presence: {:message => "Напишите текст поста!!"}
-  validates :content, :length => { :maximum => 2000, :message => "Слишком много текста !!"}
+
   validates :image, presence:  {:message => "Посту нужна обложка!!"}
   validates_format_of :image,  :with => %r{(png|jpg|jpeg)}i, :message => "Неправильный формат файла !!"
 
