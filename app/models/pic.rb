f8a96882fc9f83adee1609d0c23e4448f68e43ab
3ypup <posilay.pochashe@gmail.com>
2018-09-11 10:43:11 +0300

    add pic image album validation

--- app/models/pic.rb
+++ app/models/pic.rb
@@ -1,13 +1,14 @@
 class Pic < ApplicationRecord
 belongs_to :album
 
-
-  
-validates :images, presence: true
-validates_format_of :images,  :with => %r{(png|jpg|jpeg)}i, :message => "Неправильный формат файла !!"
-
 mount_uploaders :images, PicimageUploader
 serialize :images, JSON # If you use SQLite, add this line.
+
+
+  
+validates :images, presence: true, :on => :create
+
+validates_format_of :images,  :with => %r{(png|jpg|jpeg)}i, :message => "Неправильный формат файла !!"
 
 
 end
